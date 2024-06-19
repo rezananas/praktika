099fbe2683dafa434b6df7b1888e1a6b494707cd
rezananas <119532858+rezananas@users.noreply.github.com>
2024-06-20 00:40:38 +0300

    Add files via upload

---
+++ Praktika3.py
@@ -0,0 +1,10 @@
+# Практическая работа по уроку "Динамическая типизация"
+
+name = 'Nastya'
+print('Name:', name)
+age = 18
+print('Age:', age)
+new_age = age + 3
+print('New Age:', new_age)
+is_student = True
+print('Is Student:', is_student)
