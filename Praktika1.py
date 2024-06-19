ebef5003d0be2f5b80ac6df5c1c0d6ff33a89f51
rezananas <119532858+rezananas@users.noreply.github.com>
2024-06-20 00:01:50 +0300

    Add files via upload

---
+++ Praktika1.py
@@ -0,0 +1,8 @@
+# Практическое задание по уроку "Строки и индексация строк"
+
+example = 'Конкатенация'
+print(example[0])
+print(example[-1])
+print(example[6:])
+print(example[::-1])
+print(example[1::2])
