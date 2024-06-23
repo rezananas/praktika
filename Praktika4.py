9ce231703a5fe41ee3b9bf163724cc6d1afcb280
rezananas <119532858+rezananas@users.noreply.github.com>
2024-06-23 23:51:16 +0300

    Add files via upload

---
+++ Praktika4.py
@@ -0,0 +1,10 @@
+# Практическая работа по уроку "Организация программ и методы строк."
+
+my_string = input('В каких городах ты был во время путешествия по России? ')
+print(len(my_string))
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
