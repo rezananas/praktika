8c571952fd20e4f64568881977ae42a628fc2637
rezananas <119532858+rezananas@users.noreply.github.com>
2024-06-27 01:11:11 +0300

    Update and rename Praktika2.py to Praktika1.2.py

---
+++ Praktika1.2.py
@@ -0,0 +1,7 @@
+# Практическое задание по теме "Переменные."
+
+count_dz = 12
+count_time = 1.5
+name_course = 'Python'
+time_one_dz = count_time / count_dz
+print('Курс:', name_course, ', всего задач:', count_dz, ', затрачено часов:', count_time, ', среднее время выполнения', time_one_dz, 'часа.')
