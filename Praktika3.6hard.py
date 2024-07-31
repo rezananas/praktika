00334394e40ceeec3eb8cf4c4cc5432ccb5ab664
rezananas <119532858+rezananas@users.noreply.github.com>
2024-07-31 17:20:04 +0300

    Add files via upload

---
+++ Praktika3.6hard.py
@@ -0,0 +1,28 @@
+# Дополнительное практическое задание по модулю: "Подробнее о функциях."
+
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+def calculate_structure_sum(list_):
+  total_sum = 0
+  if isinstance(list_, (int, float)):
+    return list_
+  elif isinstance(list_, str):
+    return len(list_)
+  elif isinstance(list_, (list, tuple, set)):
+    for item in list_:
+      total_sum += calculate_structure_sum(item)
+  elif isinstance(list_, dict):
+    for key, value in list_.items():
+      total_sum += calculate_structure_sum(key)
+      total_sum += calculate_structure_sum(value)
+  return total_sum
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
